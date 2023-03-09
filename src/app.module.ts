4af92d85efcf5339d60801fe2081f58368d30ee8
Doohyeon Kim <me@doohyeon.kim>
2023-03-09 22:11:39 +0900

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { ApiController } from './api/api.controller';
+
+@Module({
+  imports: [],
+  controllers: [ApiController, AppController, UsersController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+
+@Controller("users")
+export class UsersController {
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    const { name, email } = createUserDto;
+    return `유저를 생성했습니다. 이름: ${name}, 이메일: ${email}`;
+  }
+}
